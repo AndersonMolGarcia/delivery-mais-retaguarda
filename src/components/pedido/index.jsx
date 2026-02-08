f5168289bf2b5537a4602a5d8441e5c3f50e487a
Anderson Garcia <andersonmolgarcia@gmail.com>
2026-02-08 17:03:54 -0300

    Criei o component Pedido

---
+++ src/components/pedido/index.jsx
@@ -0,0 +1,106 @@
+import './style.css';
+
+
+
+function Pedido(props) {
+
+    function Status(st) {
+        switch (st) {
+            case "P": return "Pedido em produção";
+            case "E": return "Saiu para entrega";
+            case "A": return "Aguardando...";
+            default: return "";
+        }
+    }
+
+    return <>
+        <div className='d-flex justify-content-between shadow-lg pedido me-4'>
+            <div className=''>
+                <span><b>Pedido #{props.id_pedido}</b></span>
+                <span className='badge rounded-pill bg-secondary ms-3'>{props.dt_pedido}</span>
+                <span className='badge rounded-pill bg-danger ms-3'>{Status(props.status)}</span>
+                <small className='d-block mt-1 mb-3 text-secondary'>
+                    {props.nome} - {props.endereco} -
+                    {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(props.vl_total)}
+                </small>
+
+                {
+                    props.itens.map(item => {
+                        return <div className='col-12'>
+                            <div className='row p-3 ps-0 border-top'>
+                                <div className='col-3'>
+                                    <img className='img-fluid rounded img-produto' src={item.url_foto} alt="Produto" />
+                                </div>
+
+                                <div className='col-9'>
+                                    <div className='d-flex justify-content-between align-items-center'>
+                                        <small><b>{item.nome}</b></small>
+                                        <small>
+                                            <b>
+                                                {
+                                                    new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.vl_unit * item.qtd)
+                                                }
+                                            </b>
+                                        </small>
+
+                                    </div>
+
+                                    <div className='d-block mb-2'>
+                                        {item.qtd.toLocaleString('pt-BR', { minimumIntegerDigits: 2 })} x
+                                        {
+                                            new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.vl_unit)
+                                        }
+                                    </div>
+
+                                    {
+                                        item.detalhes ?
+                                            item.detalhes.map(detalhe => {
+                                                return <small className='d-block text-secondary' key={detalhe.nome}>{detalhe.nome}</small>
+                                            })
+                                            : null
+                                    }
+                                </div>
+                            </div>
+
+
+                        </div>
+                    })
+                }
+
+            </div>
+
+            <div className='d-flex align-items-center me-4'>
+                {
+                    props.status !== "F" ?
+                        <div class="dropdown">
+                            <a 
+                                class="btn btn-secondary dropdown-toggle" 
+                                href='#'
+                                role='button'
+                                id={`dropdownMenuLink${props.id_pedido}`}
+                                data-bs-toggle="dropdown" 
+                                aria-expanded="false"
+                                
+                            >
+                                Status
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby={`dropdownMenuLink${props.id_pedido}`}>
+                                <li><a class="dropdown-item">Aguardando</a></li>
+                                <li><hr className='dropdown-divider'/></li>
+                                <li><a class="dropdown-item">Em produção</a></li>
+                                <li><a class="dropdown-item">Saiu pra entrega</a></li>
+                                <li><hr className='dropdown-divider'/></li>
+                                <li><a class="dropdown-item">Finalizar</a></li>
+                            </ul>
+                        </div>
+                        :
+                        <button><i className='fas fa-arrow-left'></i>Reabrir Pedido</button>
+                }
+            </div>
+        </div>
+    </>
+
+};
+
+
+export default Pedido;
--- src/pages/pedidos/index.jsx
+++ src/pages/pedidos/index.jsx
@@ -1,12 +1,70 @@
 import './style.css';
 import Navbar from '../../components/navbar/index.jsx';
+import Pedido from '../../components/pedido/index.jsx';
 
 
 function Pedidos() {
+
+    const pedidos = [
+  {
+    id_pedido: 524556,
+    dt_pedido: "07/02/2026 19:00:01",
+    status: "F",
+    nome: "Anderson Molina Garcia",
+    endereco: "Rua Antenor Guici, 24",
+    vl_total: "52.90",
+    itens: [
+      { id_item: 1, url_foto: "https://jornada-dev2.s3.amazonaws.com/xsalada.jpg", nome: "X-Salada", qtd: 2, vl_unit: 29, detalhes: [{nome: "Bacon"}, {nome: "Cebola"}] },
+      { id_item: 2, url_foto: "https://jornada-dev2.s3.amazonaws.com/xtudo.png", nome: "X-Tudo", qtd: 1, vl_unit: 18, detalhes: [] },
+      { id_item: 3, url_foto: "https://jornada-dev2.s3.amazonaws.com/coca-cola.png", nome: "Coca-Cola", qtd: 3, vl_unit: 12, detalhes: [] },
+      { id_item: 4, url_foto: "https://jornada-dev2.s3.amazonaws.com/dog1.png", nome: "Hot Dog", qtd: 1, vl_unit: 35, detalhes: [] },
+      { id_item: 5, url_foto: "https://jornada-dev2.s3.amazonaws.com/xbacon.jpg", nome: "X-Bacon", qtd: 2, vl_unit: 20, detalhes: [] },
+      { id_item: 6, url_foto: "https://jornada-dev2.s3.amazonaws.com/xegg.jpg", nome: "X-Egg", qtd: 1, vl_unit: 22, detalhes: [] }
+    ]
+  },
+
+  ...Array.from({ length: 19 }).map((_, i) => ({
+    id_pedido: 524557 + i,
+    dt_pedido: `07/02/2026 19:${String(5 + i).padStart(2, "0")}:00`,
+    status: ["A", "P", "E"][i % 4],
+    nome: `Cliente ${i + 2}`,
+    endereco: `Rua Exemplo, ${i + 50}`,
+    vl_total: (Math.random() * 150 + 30).toFixed(2),
+    itens: [
+      { id_item: 1, url_foto: "https://jornada-dev2.s3.amazonaws.com/xsalada.jpg", nome: "X-Salada", qtd: 2, vl_unit: 29, detalhes: [{nome: "Bacon"}, {nome: "Cebola"}] },
+      { id_item: 2, url_foto: "https://jornada-dev2.s3.amazonaws.com/xtudo.png", nome: "X-Tudo", qtd: 1, vl_unit: 18, detalhes: [] },
+      { id_item: 3, url_foto: "https://jornada-dev2.s3.amazonaws.com/coca-cola.png", nome: "Coca-Cola", qtd: 3, vl_unit: 12, detalhes: [] },
+      { id_item: 4, url_foto: "https://jornada-dev2.s3.amazonaws.com/dog1.png", nome: "Hot Dog", qtd: 1, vl_unit: 35, detalhes: [] },
+      { id_item: 5, url_foto: "https://jornada-dev2.s3.amazonaws.com/xbacon.jpg", nome: "X-Bacon", qtd: 2, vl_unit: 20, detalhes: [] },
+      { id_item: 6, url_foto: "https://jornada-dev2.s3.amazonaws.com/xegg.jpg", nome: "X-Egg", qtd: 1, vl_unit: 22, detalhes: [] }
+    ]
+  }))
+];
+
+
     return <>
-        <Navbar tela="pedidos"/>
+        <Navbar tela="pedidos" />
+
+        <div className='container-fluid mt-page'>
+            {
+                pedidos.map(pedido => {
+                    return <Pedido
+                        key={pedido.id_pedido}
+                        id_pedido={pedido.id_pedido}
+                        dt_pedido={pedido.dt_pedido}
+                        status={pedido.status}
+                        nome={pedido.nome}
+                        endereco={pedido.endereco}
+                        vl_total={pedido.vl_total}
+                        itens={pedido.itens}
+
+                    />
+                })
+            }
+
+        </div>
     </>
-    
+
 };
 
 
